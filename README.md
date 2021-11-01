dbafa2240fb57f15b8fcb1083b8a9d4e22100091
Miri <miriamkifle.dev@gmail.com>
2021-11-01 13:23:49 -0400

    tracker add

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-## Installation
+# COVID-19 Tracker
+This is an applicaiton that allows users to track their COVID symptoms. 
 
+## Tech used:
+MongoDB, Node.js, EJS, Passport, Javascript, CSS, HTML
+
+## Installation
 1. Clone repo
 2. run `npm install`
 
 ## Usage
-
 1. run `node server.js`
-2. Navigate to `localhost:8080`
+2. Navigate to `localhost:2000`
 
 ## Credit
-
 Modified from Scotch.io's auth tutorial
--- app/routes.js
+++ app/routes.js
@@ -82,9 +82,9 @@ module.exports = function(app, passport, db) {
   
           // process the login form
           app.post('/login', passport.authenticate('local-login', {
-              successRedirect : '/profile', // redirect to the secure profile section
-              failureRedirect : '/login', // redirect back to the signup page if there is an error
-              failureFlash : true // allow flash messages
+              successRedirect : '/profile', 
+              failureRedirect : '/login',
+              failureFlash : true 
           }));
   
           // SIGNUP =================================
@@ -95,9 +95,9 @@ module.exports = function(app, passport, db) {
   
           // process the signup form
           app.post('/signup', passport.authenticate('local-signup', {
-              successRedirect : '/profile', // redirect to the secure profile section
-              failureRedirect : '/signup', // redirect back to the signup page if there is an error
-              failureFlash : true // allow flash messages
+              successRedirect : '/profile',
+              failureRedirect : '/signup', 
+              failureFlash : true 
           }));
   
   // =============================================================================
